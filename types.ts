ce639a1fb5cdef554af3013d07c39f0416165b2e
Mian <mypcaccc01@gmail.com>
2025-07-09 10:14:49 -0700

    Update types.ts

--- types.ts
+++ types.ts
@@ -41,8 +41,8 @@ export interface PaginationInfo {
   };
 }
 
-export interface JikanPaginatedResponse {
-  data: Anime[];
+export interface JikanPaginatedResponse<T> {
+  data: T[];
   pagination: PaginationInfo;
 }
 
@@ -56,4 +56,69 @@ export interface Genre {
     name: string;
     url: string;
     count: number;
+}
+
+export interface Episode {
+    mal_id: number;
+    url: string;
+    title: string;
+    premium: boolean;
+}
+
+export interface ScheduleEntry {
+    entry: Anime;
+    episodes: Episode[];
+    region_locked: boolean;
+}
+
+export interface NewsArticle {
+    mal_id: number;
+    url: string;
+    title: string;
+    date: string;
+    author_username: string;
+    author_url: string;
+    forum_url: string;
+    images: {
+        jpg: {
+            image_url: string;
+        }
+    };
+    comments: number;
+    excerpt: string;
+}
+
+export interface Reviewer {
+    username: string;
+    url: string;
+    images: {
+        jpg: {
+            image_url: string;
+        }
+    }
+}
+
+export interface Review {
+    mal_id: number;
+    url: string;
+    type: string;
+    reactions: {
+        overall: number;
+        nice: number;
+        love_it: number;
+        funny: number;
+        confusing: number;
+        informative: number;
+        well_written: number;
+        creative: number;
+    };
+    date: string;
+    review: string;
+    score: number;
+    tags: string[];
+    is_spoiler: boolean;
+    is_preliminary: boolean;
+    episodes_watched: number | null;
+    user: Reviewer;
+    entry: Anime; 
 }
